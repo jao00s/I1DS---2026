a69ff33e05e96d03df5fbdd7d179c045c09e68ce
jao00s <joaofcojau@gmail.com>
2026-03-17 09:27:04 -0300

    revisao

---
+++ 07-revisao.js
@@ -0,0 +1,51 @@
+// comentario em linha 
+
+/*
+    Comentario de varias linhas
+    Bloco de codigo
+*/
+
+// Imprimir informações na tela 
+console.log("Imprime informções na telas")
+
+/*
+   Declaração de variável 
+   sintaxe: let tipo-variavel = valor variavel;
+   exemplo: let idade = 45;
+*/
+/*
+   tipos dados primarios:
+   numeros inteiros (int) = 10
+   numeros decimais (double) = 8.53
+   caracteres alfanumeros (string) = "joaozin"
+   numeros booleanos (bool) = false;
+*/
+let idade = 45;
+let salario = 2500.00
+let ehProfessor = false;
+let nome = "Joaozin Silva";
+
+console.log("Olá ", nome, "Idade", idade, "Salário", salario)
+
+/*
+   Operadores matemáticos
+   + -> Adição
+   - -> Subtração
+   * -> multiplicação
+   / -> divisão
+   % -> resto da divisão
+*/
+
+let num1 = 10;
+let num2 = 2;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let restodivisao = num1 % num2 
+
+//imprimir as Operações
+console.log("Somando", num1, " + ", num2, " = ", soma)
+console.log("O resto da divisão do", num1, "por", num2, " = ", restodivisao)
+
