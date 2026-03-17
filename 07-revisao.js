7d51f6147d7fd301c661ed324ad32c43b9232bf5
jao00s <joaofcojau@gmail.com>
2026-03-17 11:15:01 -0300

    vetor -array

--- 07-revisao.js
+++ 07-revisao.js
@@ -1,41 +1,40 @@
-// comentario em linha 
+// Comentário em linha
 
-/*
-    Comentario de varias linhas
-    Bloco de codigo
+/*  
+    Comentário de varias linhas
+    Bloco de código
 */
 
-// Imprimir informações na tela 
-console.log("Imprime informções na telas")
+// Imprimir informações ne tela
+console.log("Imprime Informações na telas");
 
 /*
-   Declaração de variável 
-   sintaxe: let tipo-variavel = valor variavel;
-   exemplo: let idade = 45;
+    Declaração de Variáveis
+    Sintaxe: let tipo-variavel = valor-variavel;
+    Exemplo: let idade = 45;
 */
 /*
-   tipos dados primarios:
-   numeros inteiros (int) = 10
-   numeros decimais (double) = 8.53
-   caracteres alfanumeros (string) = "joaozin"
-   numeros booleanos (bool) = false;
+    Tipos dados primários:
+    Numeros Inteiros (int) = 10
+    Numeros Decimais (double) = 8.53
+    Caracteres AlfaNumeros  (string) = "Cristiano"
+    Numeros Booleanos (bool) = false;
 */
 let idade = 45;
-let salario = 2500.00
-let ehProfessor = false;
-let nome = "Joaozin Silva";
+let salario = 25864.57;
+let professor = false;
+let nome = "João da Silva";
 
-console.log("Olá ", nome, "Idade", idade, "Salário", salario)
+console.log("Nome: ", nome, "Idade: ", idade, "Salário: ", salario);
 
 /*
-   Operadores matemáticos
-   + -> Adição
-   - -> Subtração
-   * -> multiplicação
-   / -> divisão
-   % -> resto da divisão
+    Operadores Matematicos
+    + -> Adição
+    - -> Subtração 
+    * -> Multiplicação
+    / -> Divisão
+    % -> Resto da Divisão
 */
-
 let num1 = 10;
 let num2 = 2;
 
@@ -43,9 +42,56 @@ let soma = num1 + num2;
 let subtracao = num1 - num2;
 let multiplicacao = num1 * num2;
 let divisao = num1 / num2;
-let restodivisao = num1 % num2 
+let restoDivisao = num1 % num2;
 
-//imprimir as Operações
-console.log("Somando", num1, " + ", num2, " = ", soma)
-console.log("O resto da divisão do", num1, "por", num2, " = ", restodivisao)
+// Imprimir as operações
+console.log("Somando ", num1, " + ", num2, " = ", soma);
+console.log("O Resto da Divisão do ", num1, " por ", num2, " = ", restoDivisao);
 
+/* 
+    Operadores de Comparação
+    == -> Comparação de Igualdade
+    != -> Diferente
+    >  -> Maior que
+    <  -> Menor que
+    >= -> Maior ou Igual
+    <= -> Menor ou Igual
+    === -> Exatamente Igual (Valor e tipo de variavel)
+
+    Retorna 'true' ou 'false'  | Verdadeiro ou Falso
+*/
+
+console.log("O Numero 1 é Igual ao Numero 2?", num1==num2);
+console.log("O Numero 1 é Maior ou Igual ao Numero 2?", num1>=num2);
+console.log("O Numero 1 é Menor que o Numero 2?", num1<num2);
+
+/* 
+    Operadores Lógicos
+
+    && (AND "E") -> Retorna verdadeiro se todas as clausulas forem verdadeiras
+    || (OR "Ou") -> Retorna verdadeiro se ao uma das clausulas forem verdadeiras
+    ! (NOT "Negação") -> Operador de Negação (Inverte o valor lógico)
+*/
+
+let sexo = "Feminino";
+let idadeAluno = 17;
+
+console.log("É sexo Masculino ou tem 17 ou mais anos de idade? ",(sexo == "Masculino") || (idadeAluno >= 17));
+
+console.log("É sexo Masculino e tem 17 ou mais anos de idade? ",(sexo == "Masculino") && (idadeAluno >= 17));
+
+console.log("É sexo Masculino e tem 17 ou mais anos de idade? ",!((sexo == "Masculino") && (idadeAluno >= 17)));
+
+
+// Concatenando strings (somando texto)
+
+let n1 = "20";
+let n2 = 10;
+
+console.log(n1+n2);
+console.log("20"+n2);
+
+var primeiroNome = "Cristiano";
+var sobreNome = " de Paula";
+
+console.log(primeiroNome+sobreNome);
