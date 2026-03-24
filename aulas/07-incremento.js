150799cefdac3b92c461935f742b20f99257447e
jao00s <joaofcojau@gmail.com>
2026-03-24 10:15:28 -0300

    incremento

--- aulas/07-incremento.js
+++ aulas/07-incremento.js
@@ -10,3 +10,17 @@ idade ++;
 console.log(idade);
 idade ++;
 console.log(idade);
+console.log("------------------------------------")
+
+//idade = idade - 1; // subtrai 1 na idade
+idade --
+console.log(idade);
+idade --;
+console.log(idade);
+console.log("------------------------------------")
+
+let novaIdade = idade + 1; // idade++ soma na mesma variavel
+console.log("Idade:", idade, "| Nova Idade:", novaIdade);
+
+novaIdade = idade++;
+console.log("Idade:", idade, "| Nova Idade:", novaIdade)
---
+++ aulas/12-lacoRepeticao.js
@@ -0,0 +1,4 @@
+//         Laço de Repetição - FOR (PARA)
+// o professor te colocou de castigo e pediu pra você escrever mil vezes a frase
+// "eu vou prestar atenção as aulas e anotar tudo!"
+
