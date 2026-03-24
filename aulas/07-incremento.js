749e52c89d532aa9f66a755f2a84542d7a2a6322
jao00s <joaofcojau@gmail.com>
2026-03-24 09:28:04 -0300

    aulas - 07incremento

---
+++ aulas/07-incremento.js
@@ -0,0 +1,12 @@
+// operadores de incremento e decremento | pré e pós incremento
+// incremento => somar algo de alguma variavel
+// decremento => subtrair algo de alguma variavel
+
+let idade = 16;
+console.log(idade);
+
+//idade = idade + 1; // somei 1 na idade 
+idade ++;
+console.log(idade);
+idade ++;
+console.log(idade);
